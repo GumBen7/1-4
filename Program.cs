7aa69461df5e5eadfc64c15a4d89c5741f90e726
Егор Сивцев <gumben7@gmail.com>
2022-12-16 11:46:36 +0700

    System added

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
 ﻿namespace OneFour {
     class Program {
         static void Main(string[] args) {
-            var nq = Console.ReadLine().Split();
-            int n = Int32.Parse(nq[0]);
-            int q = Int32.Parse(nq[1]);
+            var nq = System.Console.ReadLine().Split();
+            int n = System.Int32.Parse(nq[0]);
+            int q = System.Int32.Parse(nq[1]);
             bool[] a = new bool[n + 1];
             a[0] = true;
             a[1] = true;
-            double sqrN = Math.Sqrt(n);
+            double sqrN = System.Math.Sqrt(n);
             for (int i = 2; i <= sqrN; ++i) {
                 for (int s = i*i; s <= n; s += i) {
                     a[s] = true;                   
                 }         
             }
             for(int i = 0; i < q; ++i) {
-                string bs = Console.ReadLine(); 
-                Console.Write(bs + " "); 
-                if (a[Int32.Parse(bs)]) { 
-                    Console.WriteLine("not");
+                string bs = System.Console.ReadLine(); 
+                System.Console.Write(bs + " "); 
+                if (a[System.Int32.Parse(bs)]) { 
+                    System.Console.WriteLine("not");
                 }
                 else {
-                    Console.WriteLine("prime");
+                    System.Console.WriteLine("prime");
                 }
             }
         }
